25d911bb4ba0e48eb2a65a319b12800d7d984ab3
Raja Shravan <raja@banter.shopping>
2023-10-20 12:19:53 -0400

    used timers instead of sleep; made ui progress bar

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,21 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
     let eggTimes : [String: Int] = ["Soft": 5, "Medium": 7, "Hard": 12]
+    var timer = Timer()
+    var secondsRemaining: Int = 1
+    var cookTime: Int = 1
     
+    
+    @IBOutlet weak var progressView: UIProgressView!
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
         let hardness = sender.currentTitle!
-        print(hardness)
-        let cookTime: Int = eggTimes[hardness]!
-        for i in stride(from: cookTime, to: 0, by: -1) {
-            Thread.sleep(forTimeInterval: 1)
-            print("Time left: \(i)")
-            if (i == 1) {
-                Thread.sleep(forTimeInterval: 1)
-            }
-        }
+        secondsRemaining = eggTimes[hardness]!
+        cookTime = secondsRemaining
         
-        print("Done!")
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target:self, selector: #selector(updateTimer), userInfo:nil, repeats: true)
+        
     }
     
+    @objc func updateTimer() {
+        let timeElapsed: Int = cookTime - secondsRemaining
+        let progress: Float = Float(timeElapsed) / Float(cookTime)
+//        print("progress should be \(timeElapsed / cookTime)")
+//        print("CookTime: \(cookTime)")
+//        print("SecondsReamaining: \(secondsRemaining)")
+//        print("Progress: \(progress)")
+        progressView.setProgress(progress, animated: true)
+        
+        if secondsRemaining == 0 {
+            timer.invalidate()
+            print("Done!")
+        } else {
+            print("Time left: \(secondsRemaining)")
+            secondsRemaining -= 1
+        }
+        
+        
+    }
+
 }
